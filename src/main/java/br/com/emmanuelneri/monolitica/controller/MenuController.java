53efdc000da822db043dbcc3fb264892679ce6b3
Emmanuel Neri <emmanuel.neri@tecsinapse.com.br>
2015-04-18 15:10:27 -0300

    ajustes de projeto

--- src/main/java/br/com/emmanuelneri/monolitica/controller/MenuController.java
+++ src/main/java/br/com/emmanuelneri/monolitica/controller/MenuController.java
@@ -1,6 +1,7 @@
 package br.com.emmanuelneri.monolitica.controller;
 
 import br.com.emmanuelneri.monolitica.model.Usuario;
+import br.com.emmanuelneri.monolitica.util.ApplicationProperty;
 import br.com.emmanuelneri.monolitica.util.anotations.UsuarioLogado;
 
 import javax.enterprise.context.RequestScoped;
@@ -9,11 +10,18 @@ import javax.inject.Named;
 
 @Named
 @RequestScoped
-public class UtilController {
+public class MenuController {
+
+    @Inject
+    private ApplicationProperty applicationProperty;
 
     @Inject
     @UsuarioLogado
     private Usuario usuario;
+
+    public String getNomeProjeto() {
+        return applicationProperty.getNomeProjeto();
+    }
 
     public Usuario getUsuario() {
         return usuario;
---
+++ src/main/java/br/com/emmanuelneri/monolitica/util/producers/UsuarioProducer.java
@@ -0,0 +1,19 @@
+package br.com.emmanuelneri.monolitica.util.producers;
+
+
+import br.com.emmanuelneri.monolitica.model.Usuario;
+import br.com.emmanuelneri.monolitica.util.anotations.UsuarioLogado;
+import org.apache.shiro.SecurityUtils;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+public final class UsuarioProducer {
+
+    @Produces
+    @UsuarioLogado
+    public Usuario getUsuarioLogado() {
+        return SecurityUtils.getSubject().getPrincipals().oneByType(Usuario.class);
+    }
+
+}
